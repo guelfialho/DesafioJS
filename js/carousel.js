4f7a75c7788a04d214417a728beeef2b4a6305db
Miguel Fialho <fialhomig@gmail.com>
2022-03-26 17:46:31 -0300

    Adicionando imagem

--- js/carousel.js
+++ js/carousel.js
@@ -8,17 +8,21 @@ let carouselArr = [];
 
 //class Carousel
 class Carousel {
-
-    
+    constructor(image, title, link) {
+        this.image = image;
+        this.title = title;
+        this.link = link;
+    }
       
     static Start(arr){
+        
         if(arr){
 
             if(arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
             }
             
         } else {
@@ -27,6 +31,16 @@ class Carousel {
     }
 
     static Next(){
+       const displayCarousel = document.getElementById("carousel");
         
+       // displayCarousel.innerHTML = inserirImagem(carouselArr[0]);
+       displayCarousel.innerHTML = inserirImagem(carouselArr[0]);
     }
 };
+
+
+
+
+function inserirImagem(array){
+    return link = `<img class="selected" alt="${array.image}"src="./img/${array.image}">`
+}
