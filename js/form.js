b1aaa4eb079258d8342ed2bcbc7c80cc28d16d8e
Miguel Fialho <fialhomig@gmail.com>
2022-04-21 18:15:42 -0300

    Adicionando efeitos

--- js/form.js
+++ js/form.js
@@ -1,13 +1,15 @@
 //class contato
 
 class contato {
-    constructor(nome, sobrenome,email,cpf,telefone,contato){
+    constructor(nome, sobrenome,email,cpf,telefone,contato, tipo, mensagem){
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.email = email;
         this.cpf = cpf;
         this.telefone = telefone;
         this.contato = contato;
+        this.tipo = tipo;
+        this.mensagem = mensagem;
     }
 }
 
@@ -18,7 +20,10 @@ function Post(form) {
             form.elements.namedItem("email").value, 
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
+            form.elements.namedItem("contato").value,
+            form.elements.namedItem("tipo").value,
+            form.elements.namedItem("mensagem").value);
+
             Enviar(data);
             
 }
@@ -31,5 +36,7 @@ function Enviar(data) {
     console.log(data.cpf)
     console.log(data.telefone)
     console.log(data.contato)
+    console.log(data.tipo)
+    console.log(data.mensagem)
     alert('Obrigado sr(a) ' + data.nome + ' ' + data.sobrenome + ' os seus dados foram encaminhados com sucesso');
 }
